f4c0b7100ad003624c8a94adfc322dd8bb48ba25
luka michelson <luka.michelson@gmail.com>
2022-02-18 18:31:30 +0100

    1st commit

--- Csharp/SalesReporter.Console/Program.cs
+++ Csharp/SalesReporter.Console/Program.cs
@@ -15,27 +15,27 @@
 		if (command == "print")  
 		{  
 			 //get the header line  
-			 string line1 = dataContentString[0];  
+			 string headerCSV = dataContentString[0];  
 			 //get other content lines  
-			 var otherLines = dataContentString.Skip(1);
+			 var dataLines = dataContentString.Skip(1);
 			 var columnInfos = new List<(int index, int size, string name)>();
 			 //build the header of the table with column names from our data file  
 			 int i = 0;
-			 foreach (var columName in line1.Split(','))
+			 foreach (var columName in headerCSV.Split(','))
 			 {
 				 columnInfos.Add((i++, columName.Length, columName));
 			 }
 
-			 var headerString  = String.Join(
+			 var headerTitles  = String.Join(
 				 " | ", 
 				 columnInfos.Select(x=>x.name).Select(
 					 (val,ind) => val.PadLeft(16)));
-			 Console.WriteLine("+" + new String('-', headerString.Length + 2) + "+");
-			 Console.WriteLine("| " + headerString + " |");
-			 Console.WriteLine("+" + new String('-', headerString.Length +2 ) + "+");
+			 Console.WriteLine("+" + new String('-', headerTitles.Length + 2) + "+");
+			 Console.WriteLine("| " + headerTitles + " |");
+			 Console.WriteLine("+" + new String('-', headerTitles.Length +2 ) + "+");
 
 			 //then add each line to the table  
-			 foreach (string line in otherLines)  
+			 foreach (string line in dataLines)  
 			 { 
 				 //extract columns from our csv line and add all these cells to the line  
 				 var cells = line.Split(',');
@@ -46,7 +46,7 @@
 			            (val,ind) => val.PadLeft(16)));
 	            Console.WriteLine($"| {tableLine} |");
 			 } 
-			 Console.WriteLine("+" + new String('-', headerString.Length+2) + "+");
+			 Console.WriteLine("+" + new String('-', headerTitles.Length+2) + "+");
 
 			// if command is report
 		} 
@@ -54,9 +54,9 @@
 		{  
 		 //get all the lines without the header in the first line  
 			 var otherLines = dataContentString.Skip(1);  
-			 //declare variables for our conters  
-			 int number1 = 0, number2 = 0;  
-			 double number4 = 0.0, number5 = 0.0, number3 = 0;  
+				 //declare variables for our conters  
+			 int number1 = 0, numberOfItemsSold = 0;  
+			 double averageAmountOfBasket = 0.0, averageItemPriceSold = 0.0, totalSales = 0;  
 			 HashSet<string> clients = new HashSet<string>();  
 			 DateTime last = DateTime.MinValue;  
 			 //do the counts for each line  
@@ -65,21 +65,21 @@
 	 			var cells = line.Split(',');  
 	 			number1++;//increment the total of sales  
 	 			//to count the number of clients, we put only distinct names in a hashset //then we'll count the number of entries if (!clients.Contains(cells[1])) clients.Add(cells[1]);  
-	 			number2 += int.Parse(cells[2]);//we sum the total of items sold here  
-	 			number3 += double.Parse(cells[3]);//we sum the amount of each sell  
+	 			numberOfItemsSold += int.Parse(cells[2]);//we sum the total of items sold here  
+	 			totalSales += double.Parse(cells[3]);//we sum the amount of each sell  
 	 			//we compare the current cell date with the stored one and pick the higher last = DateTime.Parse(cells[4]) > last ? DateTime.Parse(cells[4]) : last;  
 			 } 
 			 //we compute the average basket amount per sale  
-			 number4 = Math.Round(number3 / number1,2);  
+			 averageAmountOfBasket = Math.Round(totalSales / number1,2);  
 			 //we compute the average item price sold  
-			 number5 = Math.Round(number3 / number2,2);  
+			 averageItemPriceSold = Math.Round(totalSales / numberOfItemsSold,2);  
 			 Console.WriteLine($"+{new String('-',45)}+");
 			 Console.WriteLine($"| {" Number of sales".PadLeft(30)} | {number1.ToString().PadLeft(10)} |");
 			 Console.WriteLine($"| {" Number of clients".PadLeft(30)} | {clients.Count.ToString().PadLeft(10)} |");
-			 Console.WriteLine($"| {" Total items sold".PadLeft(30)} | {number2.ToString().PadLeft(10)} |");
-			 Console.WriteLine($"| {" Total sales amount".PadLeft(30)} | {Math.Round(number3,2).ToString().PadLeft(10)} |");
-			 Console.WriteLine($"| {" Average amount/sale".PadLeft(30)} | {number4.ToString().PadLeft(10)} |");
-			 Console.WriteLine($"| {" Average item price".PadLeft(30)} | {number5.ToString().PadLeft(10)} |");
+			 Console.WriteLine($"| {" Total items sold".PadLeft(30)} | {numberOfItemsSold.ToString().PadLeft(10)} |");
+			 Console.WriteLine($"| {" Total sales amount".PadLeft(30)} | {Math.Round(totalSales,2).ToString().PadLeft(10)} |");
+			 Console.WriteLine($"| {" Average amount/sale".PadLeft(30)} | {averageAmountOfBasket.ToString().PadLeft(10)} |");
+			 Console.WriteLine($"| {" Average item price".PadLeft(30)} | {averageItemPriceSold.ToString().PadLeft(10)} |");
 			 Console.WriteLine($"+{new String('-',45)}+");
 		}  
 		else  
